abb21837aaf50fa55cf3c55269e181678fee98da
boroda123 <boroda_123@yahoo.com>
2024-06-27 17:27:24 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 
 import UserList from './UserList.js';
 import UserDetails from "./UserDetails";
+import React from "react";
 
 function App() {
 
     return (
         <div className="App">
-          <UserList />
-          <br />
-          <UserDetails />
+            <h2>User List</h2>
+            <UserList/>
+            <br />
+            <UserDetails/>
         </div>
     );
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h2>User List</h2>
     <App />
   </React.StrictMode>
 );
