30292cb370c2de5a0e6a56b6f830d9d08e5a4ea4
boroda123 <boroda_123@yahoo.com>
2024-07-01 15:05:05 -0700

    Tuning Docker build

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import UserList from './UserList.js';
 import UserDetails from "./UserDetails";
 import React from "react";
 
-const REACT_APP_ENV = 'Default';
+const REACT_APP_ENV = 'dev';
+const REACT_APP_DEP = 'Local';
 
 function App() {
     let appEnv = REACT_APP_ENV;
     if (process.env.REACT_APP_ENV) {
         appEnv = process.env.REACT_APP_ENV;
     }
+    let appDep = REACT_APP_DEP;
+    if (process.env.REACT_APP_DEP) {
+        appEnv = process.env.REACT_APP_DEP;
+    }
 
     return (
         <div className="App">
-            <h2>User List ({appEnv}):</h2>
+            <h2>User List ({appEnv}, {appDep}):</h2>
             <UserList/>
             <br />
             <UserDetails/>
