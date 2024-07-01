55ff23ea7019d51dbf67a9d6e8d8161a6b6e7296
boroda123 <boroda_123@yahoo.com>
2024-07-01 12:51:19 -0700

    Modify Docker build to include build-arg

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import UserList from './UserList.js';
 import UserDetails from "./UserDetails";
 import React from "react";
 
+const REACT_APP_ENV = 'Default';
+
 function App() {
+    let appEnv = REACT_APP_ENV;
+    if (process.env.REACT_APP_ENV) {
+        appEnv = process.env.REACT_APP_ENV;
+    }
 
     return (
         <div className="App">
-            <h2>User List</h2>
+            <h2>User List ({appEnv}):</h2>
             <UserList/>
             <br />
             <UserDetails/>
